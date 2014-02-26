c0371ccda07c4974d8641d8f69ec3e8a7ef8330c
Daniel Pasternak <danjpasternak@gmail.com>
2014-02-25 21:59:13 -0500

    added birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -9,6 +9,12 @@ function Draw(){
     context.drawImage(spritesheet, 0, 270, 80, 120, 00, 70, 280, 420); //tree
     context.drawImage(spritesheet, 100, 700,800,200,0,400, 800,200);  //ground
     context.drawImage(spritesheet, 0, 0, 60, 50, 200, 450, 180, 120); //dog
-
+    
+    //5 birds
+    context.drawImage(spritesheet,0, 115, 36, 36, 200, 30, 72, 72);
+    context.drawImage(spritesheet, 0, 115, 36, 36, 400, 100, 72, 72);
+    context.drawImage(spritesheet,0, 115, 36, 36, 550, 45, 72, 72);
+    context.drawImage(spritesheet,0,115,36,36,250, 200, 72, 72);
+    context.drawImage(spritesheet,0,115,36,36,650, 300, 72, 72);
   };
 };		

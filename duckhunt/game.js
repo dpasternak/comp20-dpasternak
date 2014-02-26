bafe420e92fdd4563b14aed0c3f33fb7e0526fa6
Daniel Pasternak <danjpasternak@gmail.com>
2014-02-25 21:30:08 -0500

    Updated game.js with Draw function that draws ground and tree

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,10 @@
-// Your work goes here...
+  function Draw(){
+  var canvas = document.getElementById("game");
+  var context = canvas.getContext("2d");
+  var ground = new Image();
+  ground.src = "assets/duckhunt.png";
+  ground.onload = function() {
+    context.drawImage(ground, 0, 270, 80, 120, 00, 70, 280, 420);
+    context.drawImage(ground, 100, 700,800,200,0,400, 800,200  );
+      };
+};	

d87bb83edfb363fa29303ca2648787d01fa1b538
Daniel Pasternak <danjpasternak@gmail.com>
2014-02-25 21:49:55 -0500

    disabled image smoothing to make scaled images sharper

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,10 +1,14 @@
-  function Draw(){
+function Draw(){
   var canvas = document.getElementById("game");
   var context = canvas.getContext("2d");
-  var ground = new Image();
-  ground.src = "assets/duckhunt.png";
-  ground.onload = function() {
-    context.drawImage(ground, 0, 270, 80, 120, 00, 70, 280, 420);
-    context.drawImage(ground, 100, 700,800,200,0,400, 800,200  );
-      };
-};	
+  var spritesheet = new Image();
+  spritesheet.src = "assets/duckhunt.png";
+  spritesheet.onload = function() {
+  	context.imageSmoothingEnabled = false; 
+
+    context.drawImage(spritesheet, 0, 270, 80, 120, 00, 70, 280, 420); //tree
+    context.drawImage(spritesheet, 100, 700,800,200,0,400, 800,200);  //ground
+    context.drawImage(spritesheet, 0, 0, 60, 50, 200, 450, 180, 120); //dog
+
+  };
+};		
